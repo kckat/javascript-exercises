3109a3290510155350fb54d5cc09a4c00066eeb1
kckat <kaitlyn.celine@gmail.com>
2020-07-27 10:49:59 -0500

    used forEach loop to filter out elements

--- removeFromArray/removeFromArray.js
+++ removeFromArray/removeFromArray.js
@@ -1,5 +1,13 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(...args) {
+    let originalArray = args[0]
+    let newArray = []
+    originalArray.forEach(item => {
+        if(!args.includes(item)){
+            newArray.push(item)
+        }
+    });
+    return newArray
 }
+console.log(removeFromArray([1, 2, 3, 4], 3, 2))
 
 module.exports = removeFromArray
