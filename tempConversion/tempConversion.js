33b18d28a9d3447c54b6a649cabb00e461887b09
kckat <kaitlyn.celine@gmail.com>
2020-07-27 11:46:08 -0500

    created temperature unit conversion js function

--- tempConversion/tempConversion.js
+++ tempConversion/tempConversion.js
@@ -1,10 +1,17 @@
-const ftoc = function() {
-
+const ftoc = function(number) {
+  let celcius = ((number - 32)*(5/9))
+  console.log(celcius)
+  celcius = Math.round(celcius * 10)/ 10
+  return celcius;
 }
 
-const ctof = function() {
-
+const ctof = function(number) {
+  let farenheight = ((number * (9/5))+ 32)
+  farenheight = Math.round(farenheight * 10)/ 10
+  return farenheight;
 }
+
+console.log(ftoc(32))
 
 module.exports = {
   ftoc,
