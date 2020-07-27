79c154eeae5dea3f3ff0593083304aa5f8dce375
kckat <kaitlyn.celine@gmail.com>
2020-07-27 09:42:10 -0500

    created reverse string array

--- reverseString/reverseString.js
+++ reverseString/reverseString.js
@@ -1,5 +1,13 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+    let newString = ""
+    let stringArray = string.split("")
+    let newStringArray = []
+    for (let i=0; i < stringArray.length; i++){
+        newStringArray.unshift(stringArray[i])
+    }
+    newString = newStringArray.join("")
+    return newString
 }
+
 
 module.exports = reverseString
