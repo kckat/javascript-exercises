6a9551e966054124fbeeba7580e0c1f54a9f0b0d
kckat <kaitlyn.celine@gmail.com>
2020-07-27 09:21:43 -0500

    added repeat string function and tested

--- repeatString/repeatString.js
+++ repeatString/repeatString.js
@@ -1,5 +1,13 @@
-const repeatString = function() {
-
+const repeatString = function(string, numberOfRepeats) {
+    let newString = ''
+    if(numberOfRepeats < 0) {
+        return "ERROR"
+    } else {
+        for (let i = 0; i < numberOfRepeats; i++){
+            newString += string
+        }
+    }
+    return newString
 }
 
 module.exports = repeatString
