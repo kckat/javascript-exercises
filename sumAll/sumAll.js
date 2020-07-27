fc01ea3b5445e9f3a155270d17a34d3816ca54fe
kckat <kaitlyn.celine@gmail.com>
2020-07-27 11:21:04 -0500

    sum All method in js

--- sumAll/sumAll.js
+++ sumAll/sumAll.js
@@ -1,5 +1,20 @@
-const sumAll = function() {
-
+const sumAll = function(min, max) {
+    if (min <= 0 || max <= 0) {return "ERROR"}
+    if (typeof(min) !== "number" || typeof(max) !== "number") {return "ERROR"}
+    
+    if (min > max){
+        let tempA = min
+        let tempB = max
+        max = tempA
+        min = tempB
+    }
+    finalSum = 0
+    for(let i = min; i <= max; i++){
+        finalSum += i
+    }
+    return finalSum
 }
+
+console.log(sumAll(123, 1))
 
 module.exports = sumAll
